962f9487cf3fd776c45a770be1e9361c0bffe558
Tommy Han <tommy_han1@intuit.com>
2022-03-14 17:05:12 -0400

    refactor

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // change the entry to use different app.
-  entry: "./src/shoppingCart/index.js",
+  entry: "./src/functionalAstronauts/index.js",
   output: {
     filename: "index.bundle.js",
     path: path.join(__dirname, "/dist"),
