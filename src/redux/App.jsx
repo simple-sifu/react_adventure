3175bb02596e10aec431411e76855f655d5b5a10
Tommy Han <tommy_han1@intuit.com>
2022-03-25 14:14:27 -0400

    refactor

--- src/redux/App.jsx
+++ src/redux/App.jsx
@@ -1,45 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import App from './App';
 
-// Action Increment
-const incrementAction = () => {
-  return {
-    type: 'INCREMENT'
-  };
-};
-const decrementAction = () => {
-  return {
-    type: 'DECREMENT'
-  };
-};
+class App extends React.Component {
 
-// Reducer
-const counterReducer = (state=0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1
-    }
+render(){
+  return (
+    <div>Hello World</div>
+  )
 }
 
-let store = createStore(counterReducer);
+}
 
-// Display it in console
-store.subscribe(() => console.log(store.getState()));
-
-
-// DISPATCH
-store.dispatch(incrementAction());
-store.dispatch(incrementAction());
-store.dispatch(decrementAction());
-
-
-// Dispatch
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+export default App
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,47 @@
 require("file-loader?name=[name].[ext]!../index.html");
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+// Action Increment
+const incrementAction = () => {
+  return {
+    type: 'INCREMENT'
+  };
+};
+const decrementAction = () => {
+  return {
+    type: 'DECREMENT'
+  };
+};
+
+// Reducer
+const counterReducer = (state=0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1
+    }
+}
+
+let store = createStore(counterReducer);
+
+// Display it in console
+store.subscribe(() => console.log(store.getState()));
+
+
+// DISPATCH
+store.dispatch(incrementAction());
+store.dispatch(incrementAction());
+store.dispatch(decrementAction());
+
+
+// Dispatch
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
